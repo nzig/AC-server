b95e738c9e5138c6448088c0fcc55f07ef549678
nzig <7372858+nzig@users.noreply.github.com>
2017-07-04 01:10:39 +0300

    prettier

--- hello.go
+++ hello.go
@@ -15,41 +15,8 @@ import (
 const projectName string = "kodicloud-169614"
 
 func init() {
-	http.HandleFunc("/", root)
 	http.HandleFunc("/send", send)
 }
-
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, mainHTML)
-}
-
-const mainHTML = `
-<html>
-	<body>
-		<form action="/send" method="post">
-		<select name="temp">
-			<option>off</option>
-			<option>16</option>
-			<option>17</option>
-			<option>18</option>
-			<option>19</option>
-			<option>20</option>
-			<option>21</option>
-			<option>22</option>
-			<option>23</option>
-			<option>24</option>
-			<option>25</option>
-			<option>26</option>
-			<option>27</option>
-			<option>28</option>
-			<option>29</option>
-			<option>30</option>
-		</select> 
-		<input type="submit" value="Send">
-		</form>
-	</body>
-</html>
-`
 
 func send(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
--- static/script.js
+++ static/script.js
@@ -1,10 +1,14 @@
 function sendData(){
     let temp = document.getElementById('temp').value;
+    let formData = new FormData();
+    formData.append('temp', temp);
     fetch('/send', {
         method: 'POST',
         credentials: 'include',
-        body: temp
+        body: formData
     }).then(r => r.text()).then(t => {
-        document.getElementById('output').innerText = t;
+        let out =  document.getElementById('output');
+        out.innerText = t;
+        out.classList.add('in');
     })
 }
