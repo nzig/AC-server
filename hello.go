cc3179155959ee29b7b29bfa5ece8a7e1706e706
nzig <7372858+nzig@users.noreply.github.com>
2018-09-27 13:58:13 +0300

    Add email address to message

--- hello.go
+++ hello.go
@@ -21,17 +21,14 @@ func init() {
 func send(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
-	switch u.Email {
-	case "nadavz0@gmail.com":
-	case "dor.israeli@gmail.com":
-	case "giljpeled@gmail.com":
-
-	default:
-		http.Error(w, "Not allowed", http.StatusForbidden)
+	if !isAllowedUser(u.Email) {
+		errMsg := fmt.Sprintf("%s Not Allowed", u.Email)
+		http.Error(w, errMsg, http.StatusForbidden)
 		return
 	}
-	temp := r.FormValue("temp")
-	log.Infof(c, "[%s] executed %s", u.Email, temp)
+
+	temperature := r.FormValue("temp")
+	log.Infof(c, "[%s] executed %s", u.Email, temperature)
 	client, err := pubsub.NewClient(c, projectName)
 	if err != nil {
 		http.Error(w, "Failed to create client", http.StatusForbidden)
@@ -42,11 +39,26 @@ func send(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to get topic", http.StatusForbidden)
 		return
 	}
-	result := topic.Publish(c, &pubsub.Message{Data: []byte(temp)})
+	result := topic.Publish(c, &pubsub.Message{
+		Data: []byte(temperature),
+		Attributes: map[string]string{
+			"user": u.Email,
+		},
+	})
 	_, err = result.Get(c)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "Sent %s", template.HTMLEscapeString(temp))
+	fmt.Fprintf(w, "Sent %s", template.HTMLEscapeString(temperature))
+}
+
+func isAllowedUser(email string) bool {
+	switch email {
+	case "nadavz0@gmail.com",
+		"dor.israeli@gmail.com",
+		"talflom@gmail.com":
+		return true
+	}
+	return false
 }
